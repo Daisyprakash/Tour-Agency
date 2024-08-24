4f57f8694d22bea43c1e00ef920688f77cfbbac1
Daisyprakash17 <daisyprakash007@gmail.com>
2024-08-25 03:13:48 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -28,16 +28,24 @@ const app = express();
 //* Middlewares ****************************************************
 
 // Implement cors
-app.use(
-  cors({
-    origin: ["http://localhost:5173", process.env.FRONTEND_URL,"*"],
-    credentials: true,
-  })
-);
-app.options("*", cors());
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173", process.env.FRONTEND_URL,"*"],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE',"PATCH"],
+//     credentials: true,
+//   })
+// );
+// app.options("*", cors());
 
-// set security HTTP headers
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+// // set security HTTP headers
+// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+app.use((req, res, next) => {
+  console.log('CORS Middleware Triggered');
+  console.log('Origin:', req.headers.origin);
+  next();
+});
+
 
 // Logger for dev
 if (process.env.NODE_ENV === "development") {
