1263ad0ba774562d8ad519730cb06e2e2769848d
Daisyprakash17 <daisyprakash007@gmail.com>
2024-08-25 02:59:39 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173, // Set the port for the Vite development server
-    proxy: {
-      '/api': {
-        target: 'http://localhost:9000', // Your backend server
-        changeOrigin: true,
-        secure: false, // Set to true if your backend uses HTTPS
-      },
-    },
   },
 });
