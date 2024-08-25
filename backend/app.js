875f479ce4652605bb106a7f27aca2ca9ec8ec8a
Daisyprakash17 <daisyprakash007@gmail.com>
2024-08-26 00:53:15 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ const app = express();
 // Implement cors
 app.use(
   cors({
-    origin: ["http://localhost:5173", process.env.FRONTEND_URL,"https://tour-agency-kboj.onrender.com"],
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
     methods: ['GET', 'POST', 'PUT', 'DELETE',"PATCH","OPTIONS"],
     credentials: true,
   })
