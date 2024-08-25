8d404ca37fd1e955aae751eb9b9b733922aa8aae
Daisyprakash17 <daisyprakash007@gmail.com>
2024-08-26 02:31:33 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import UnprotectedRoute from "./components/layout/UnprotectedRoute";
-import ProtectedRoute from "./components/layout/ProtectedRoute";
 import RootLayout from "./components/layout/RootLayout";
 import Error from "./components/layout/Error";
+import UnprotectedRoute from "./components/layout/UnprotectedRoute";
+import ProtectedRoute from "./components/layout/ProtectedRoute";
 
 import Overview from "./pages/tour/Overview";
 import TourDetails from "./pages/tour/TourDetails";
@@ -28,188 +27,94 @@ import ManageTours from "./pages/management/ManageTours";
 import CreateTour from "./pages/management/CreateTour";
 import EditTour from "./pages/management/EditTour";
 
-export default function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout />,
-      errorElement: <Error />,
-      children: [
-        {
-          index: true,
-          element: (
-            <UnprotectedRoute>
-              <Overview />
-            </UnprotectedRoute>
-          ),
-        },
-        {
-          path: "tours/:id",
-          element: (
-            <UnprotectedRoute>
-              <TourDetails />
-            </UnprotectedRoute>
-          ),
-        },
-        {
-          path: "verifyCode",
-          element: (
-            <UnprotectedRoute>
-              <CodeVerification />
-            </UnprotectedRoute>
-          ),
-        },
-        {
-          // Auth routes
-          path: "auth",
-          element: <ProtectedRoute reverse />,
-          children: [
-            {
-              path: "signup",
-              element: <Signup />,
-            },
-            {
-              path: "login",
-              element: <Login />,
-            },
-            {
-              path: "twoFactor/authApp",
-              element: <ValidateTwoFactor />,
-            },
-            {
-              path: "twoFactor/chooseMethod",
-              element: <ChooseTwoFactorMethod />,
-            },
-            {
-              path: "forgotPassword",
-              element: <ForgotPassword />,
-            },
-            {
-              path: "passwordReset/:token?",
-              element: <ResetPassword />,
-            },
-          ],
-        },
-        {
-          // Me routes
-          path: "me",
-          children: [
-            {
-              path: "profile",
-              children: [
-                {
-                  index: true,
-                  element: (
-                    <ProtectedRoute>
-                      <MyDetails />
-                    </ProtectedRoute>
-                  ),
-                },
-                {
-                  path: "update",
-                  element: (
-                    <ProtectedRoute type="provider" restrictTo={["local"]}>
-                      <UpdateMyDetails />
-                    </ProtectedRoute>
-                  ),
-                },
-              ],
-            },
-            {
-              path: "security",
-              element: (
-                <ProtectedRoute>
-                  <Security />
-                </ProtectedRoute>
-              ),
-            },
-            {
-              path: "bookings",
-              element: (
-                <ProtectedRoute>
-                  <MyBookings />
-                </ProtectedRoute>
-              ),
-            },
-            {
-              path: "reviews",
-              element: (
-                <ProtectedRoute type="role" restrictTo={["user"]}>
-                  <MyReviews />
-                </ProtectedRoute>
-              ),
-            },
-          ],
-        },
-        {
-          // Manage  routes
-          path: "manage",
-          children: [
-            {
-              path: "tours",
-              element: (
-                <ProtectedRoute
-                  type="role"
-                  restrictTo={["admin", "lead-guide"]}
-                />
-              ),
-              children: [
-                {
-                  index: true,
-                  element: <ManageTours />,
-                },
-                {
-                  path: "create",
-                  element: <CreateTour />,
-                },
-                {
-                  path: "edit/:id",
-                  element: <EditTour />,
-                },
-              ],
-            },
-            {
-              path: "users",
-              element: <ProtectedRoute type="role" restrictTo={["admin"]} />,
-              children: [
-                {
-                  index: true,
-                  element: <ManageUsers />,
-                },
-                {
-                  path: "profile/:id",
-                  element: <UserDetails />,
-                },
-                {
-                  path: "edit/:id",
-                  element: <EditUser />,
-                },
-              ],
-            },
-            {
-              path: "reviews",
-              element: (
-                <ProtectedRoute type="role" restrictTo={["admin"]}>
-                  <ManageReviews />
-                </ProtectedRoute>
-              ),
-            },
-            {
-              path: "bookings",
-              element: (
-                <ProtectedRoute
-                  type="role"
-                  restrictTo={["admin", "lead-guide"]}
-                >
-                  <ManageBookings />
-                </ProtectedRoute>
-              ),
-            },
-          ],
-        },
-      ],
-    },
-  ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: (
+          <UnprotectedRoute>
+            <Overview />
+          </UnprotectedRoute>
+        ),
+      },
+      {
+        path: "tours/:id",
+        element: (
+          <UnprotectedRoute>
+            <TourDetails />
+          </UnprotectedRoute>
+        ),
+      },
+      {
+        path: "verifyCode",
+        element: (
+          <UnprotectedRoute>
+            <CodeVerification />
+          </UnprotectedRoute>
+        ),
+      },
+      {
+        path: "auth",
+        element: <ProtectedRoute reverse />,
+        children: [
+          { path: "signup", element: <Signup /> },
+          { path: "login", element: <Login /> },
+          { path: "twoFactor/authApp", element: <ValidateTwoFactor /> },
+          { path: "twoFactor/chooseMethod", element: <ChooseTwoFactorMethod /> },
+          { path: "forgotPassword", element: <ForgotPassword /> },
+          { path: "passwordReset/:token?", element: <ResetPassword /> },
+        ],
+      },
+      {
+        path: "me",
+        element: <ProtectedRoute />,
+        children: [
+          {
+            path: "profile",
+            children: [
+              { index: true, element: <MyDetails /> },
+              { path: "update", element: <UpdateMyDetails /> },
+            ],
+          },
+          { path: "security", element: <Security /> },
+          { path: "bookings", element: <MyBookings /> },
+          { path: "reviews", element: <MyReviews /> },
+        ],
+      },
+      {
+        path: "manage",
+        element: <ProtectedRoute type="role" restrictTo={["admin"]} />,
+        children: [
+          {
+            path: "tours",
+            element: <ManageTours />,
+            children: [
+              { index: true, element: <ManageTours /> },
+              { path: "create", element: <CreateTour /> },
+              { path: "edit/:id", element: <EditTour /> },
+            ],
+          },
+          {
+            path: "users",
+            element: <ManageUsers />,
+            children: [
+              { index: true, element: <ManageUsers /> },
+              { path: "profile/:id", element: <UserDetails /> },
+              { path: "edit/:id", element: <EditUser /> },
+            ],
+          },
+          { path: "reviews", element: <ManageReviews /> },
+          { path: "bookings", element: <ManageBookings /> },
+        ],
+      },
+    ],
+  },
+]);
 
+export default function App() {
   return <RouterProvider router={router} />;
 }
